37c2c4bdb195d4f9d625846ecd7756f173fd424a
DESKTOP-5BSHC8B\Lakshit <lakshitnagar@gmail.com>
2018-10-12 00:44:45 +0530

    new node

--- Linked Lists/Singly_List_List.c
+++ Linked Lists/Singly_List_List.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
 
-int main(void) {
-	// your code goes here
-	return 0;
+struct ListNode {
+    int data;
+    struct ListNode *next;
+};
+
+struct ListNode *createNewNode(){
+    struct ListNode *newNode;
+    newNode = malloc(sizeof(struct ListNode));
+
+    return newNode;
 }
 
+int main()
+{
+    struct ListNode *head;
+    head = createNewNode();
+    head->data = 2;
+    printf("Hello World %d", head->data);
+
+    return 0;
+}
