6f2f4e58d42289eb6668b97943c0d0e257a5ab75
DESKTOP-5BSHC8B\Lakshit <lakshitnagar@gmail.com>
2018-10-23 08:35:15 +0530

    cyclic list generation and iteration

--- Link_Lists/Cyclic_List.c
+++ Link_Lists/Cyclic_List.c
@@ -18,18 +18,76 @@ struct ListNode *createNewNode(){
 }
 
 struct ListNode *generateCyclicList(int loopStartPoint, int loopSize){
+    if(loopStartPoint<1 || loopSize<0) return NULL;
     struct ListNode *head = createNewNode();
     if(!head) return NULL;
 
     struct ListNode *currNode = head;
+    struct ListNode *nextNode, *loopStartNode;
 
-    for(int i=1; i<=loopStartPoint; i++){
+    for(int i=1; i<loopStartPoint; i++){
+        nextNode = createNewNode();
+        if(!nextNode) return NULL;
+
+        printf("currNode %d\n", currNode);
+        currNode->next = nextNode;
+        currNode->data = i*i;
+
+        currNode = nextNode;
+    }
+
+    loopStartNode = currNode;
+    printf("loopStartNode %d\n", loopStartNode);
+
+    for(int i=1; i<loopSize; i++){
+        nextNode = createNewNode();
+        if(!nextNode) return NULL;
         
+        printf("currNode %d\n", currNode);
+        currNode->next = nextNode;
+        currNode->data = i*i;
+
+        currNode = nextNode;
+    }
+
+    currNode->next = loopStartNode;
+    printf("currNode %d\n", currNode);
+    printf("currNode->next %d\n\n", currNode->next);
+
+    return head;
+}
+
+void iterateCyclicList(struct ListNode *head, int loopStartPoint){
+    if(!head) return NULL;
+
+    struct ListNode *currNode = head;
+    struct ListNode *loopStartNode;
+    int count = 1;
+
+    while(count<loopStartPoint){
+        printf("Node at %dth location: %d\n", count, currNode);
+        currNode = currNode->next;
+        count++;
+    }
+
+    loopStartNode = currNode;
+    if(loopStartNode==currNode)
+    printf("Node at %dth location(loop start): %d\n", count, currNode);
+    currNode = currNode->next;
+    count++;
+
+    while(currNode!=loopStartNode){
+        printf("Node at %dth location(inside loop): %d\n", count, currNode);
+        currNode = currNode->next;
+        count++;
     }
 }
 
 int main() {
-
+    int loopStart = 17;
+    int loopSize = 10;
+    struct ListNode *head = generateCyclicList(loopStart, loopSize);
+    iterateCyclicList(head, loopStart);
 
     return 0;
 }
