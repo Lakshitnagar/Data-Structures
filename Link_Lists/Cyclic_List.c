31d47d790a98522eedb4d7af9b6913a492fe9661
PERSISTENT\lakshit_nagar <lakshit_nagar@persistent.co.in>
2018-10-23 14:49:49 +0530

    loopsize handle for less than 0and is Cyclic function implemented

--- Link_Lists/Cyclic_List.c
+++ Link_Lists/Cyclic_List.c
@@ -18,7 +18,11 @@ struct ListNode *createNewNode(){
 }
 
 struct ListNode *generateCyclicList(int loopStartPoint, int loopSize){
-    if(loopStartPoint<1 || loopSize<0) return NULL;
+    if(loopStartPoint<1){
+        printf("Loop starting point must be at least from begining\n");
+        return NULL;
+    }
+
     struct ListNode *head = createNewNode();
     if(!head) return NULL;
 
@@ -29,65 +33,90 @@ struct ListNode *generateCyclicList(int loopStartPoint, int loopSize){
         nextNode = createNewNode();
         if(!nextNode) return NULL;
 
-        printf("currNode %d\n", currNode);
         currNode->next = nextNode;
-        currNode->data = i*i;
+        currNode->data = i;
+        printf("currNode %d\n", currNode->data);
 
         currNode = nextNode;
     }
 
     loopStartNode = currNode;
     printf("loopStartNode %d\n", loopStartNode);
-
+    
     for(int i=1; i<loopSize; i++){
         nextNode = createNewNode();
         if(!nextNode) return NULL;
         
-        printf("currNode %d\n", currNode);
         currNode->next = nextNode;
-        currNode->data = i*i;
+        currNode->data = i;
+        printf("currNode %d\n", currNode->data);
 
         currNode = nextNode;
     }
 
-    currNode->next = loopStartNode;
-    printf("currNode %d\n", currNode);
-    printf("currNode->next %d\n\n", currNode->next);
+    if(loopSize<1)
+        currNode->next = NULL;
+    else
+        currNode->next = loopStartNode;
+    currNode->data = 9876;
+    printf("currNode %d\n", currNode->data);
 
     return head;
 }
 
 void iterateCyclicList(struct ListNode *head, int loopStartPoint){
-    if(!head) return NULL;
+    if(!head){
+        printf("Head value is NULL\n");
+        return NULL;
+    }
 
     struct ListNode *currNode = head;
     struct ListNode *loopStartNode;
     int count = 1;
 
     while(count<loopStartPoint){
-        printf("Node at %dth location: %d\n", count, currNode);
+        printf("Node at %dth location: %d\n", count, currNode->data);
         currNode = currNode->next;
         count++;
     }
 
     loopStartNode = currNode;
     if(loopStartNode==currNode)
-    printf("Node at %dth location(loop start): %d\n", count, currNode);
+    printf("Node at %dth location(loop start): %d\n", count, currNode->data);
     currNode = currNode->next;
     count++;
 
-    while(currNode!=loopStartNode){
-        printf("Node at %dth location(inside loop): %d\n", count, currNode);
+    while(currNode!=loopStartNode && currNode!=NULL){
+        printf("Node at %dth location(inside loop): %d\n", count, currNode->data);
         currNode = currNode->next;
         count++;
     }
 }
 
+int isCyclicList(struct ListNode *head){
+    if(!head) return NULL;
+
+    struct ListNode *slowPtr, *fastPtr;
+    slowPtr = fastPtr = head;
+    slowPtr = slowPtr->next;
+    fastPtr = fastPtr->next->next;
+
+    while(slowPtr!=fastPtr && fastPtr->next!=NULL){
+        slowPtr = slowPtr->next;
+        fastPtr = fastPtr->next->next;
+    }
+
+    if(slowPtr==fastPtr) return 1;
+    else return 0;
+}
+
 int main() {
-    int loopStart = 17;
-    int loopSize = 10;
+    int loopStart = 15;
+    int loopSize = 0;
     struct ListNode *head = generateCyclicList(loopStart, loopSize);
     iterateCyclicList(head, loopStart);
+
+    printf("\nLoop status: %d\n", isCyclicList(head));
 
     return 0;
 }
