c179bd87d531997db213ab60d68eaa53d1852525
Lakshitnagar <lakshitnagar@gmail.com>
2018-11-09 22:04:56 +0530

    preorder completed

--- Trees/Pre_Order_Traversal.c
+++ Trees/Pre_Order_Traversal.c
@@ -12,7 +12,7 @@ struct Queue {
     struct QueueNode *rear;
 };
 
-struct QueueNode *createNewNode(){
+struct QueueNode *createNewQueueNode(){
     struct QueueNode *newNode = (struct QueueNode *)malloc(sizeof(struct QueueNode));
     if(!newNode) return NULL;
     else return newNode;
@@ -90,7 +90,7 @@ void iterateQueue(struct QueueNode *head){
 
 }
 
-int ListLength(struct QueueNode *head){
+int QueueLength(struct QueueNode *head){
     if(!head) return 0;
 
     int count = 0;
@@ -102,6 +102,79 @@ int ListLength(struct QueueNode *head){
     }
 
     return count;
+}
+
+struct StackNode{
+    struct BinaryTreeNode *BTNode;
+    struct StackNode *next;
+};
+
+struct StackNode *createNewStackNode(){
+    struct StackNode *newNode = (struct StackNode *)malloc(sizeof(struct StackNode));
+    if(!newNode) return NULL;
+    else return newNode;
+}
+
+struct StackNode *createNewStack(){
+    return NULL;
+}
+
+void iterateStack(struct StackNode *head){
+    struct StackNode *currNode, *nextNode;
+
+    if(!head) return;
+    
+    currNode = head;
+    nextNode = currNode->next;
+
+    int i=0;
+    while(nextNode){
+        printf("Data at %dth location : %d \n", i, currNode->BTNode);
+        currNode = nextNode;
+        nextNode = nextNode->next;
+        i++;
+    }
+    printf("Data at %dth location : %d \n \n", i, currNode->BTNode);
+
+}
+
+int StackLength(struct StackNode *head){
+    if(!head) return 0;
+
+    int count = 0;
+    struct StackNode *currNode = head;
+
+    while(currNode!=NULL){
+        count++;
+        currNode = currNode->next;
+    }
+
+    return count;
+}
+
+void push(struct StackNode **top, struct BinaryTreeNode *BTNode){
+    struct StackNode *newStackNode = createNewStackNode();
+    if(!newStackNode) return;
+
+    newStackNode->BTNode = BTNode;
+    newStackNode->next = *top;
+    *top = newStackNode;
+}
+
+int isEmptyStack(struct StackNode *top){
+    return top == NULL;
+}
+
+struct BinaryTreeNode *pop(struct StackNode **top){
+    if(isEmptyStack(*top)) return NULL;
+
+    struct BinaryTreeNode *data;
+    struct StackNode *temp = *top;
+    data = temp->BTNode;
+    *top = temp->next;
+    free(temp);
+
+    return data;
 }
 
 struct BinaryTreeNode{
@@ -166,7 +239,7 @@ struct BinaryTreeNode *createFullBinaryTree_iterative(int depth){
     BTRoot->data = count++;
     enQueue(Q, BTRoot);
 
-    while(ListLength(Q->front) < pow(2, depth)){
+    while(QueueLength(Q->front) < pow(2, depth)){
         struct BinaryTreeNode *FrontBTNode = Q->front->BTNode;
         struct BinaryTreeNode *left, *right;
 
@@ -213,11 +286,26 @@ void preOrderTraversal_Recursive(struct BinaryTreeNode *root){
     }
 }
 
+void preOrderTraversal_Iterative(struct BinaryTreeNode *root){
+    struct StackNode *S = createNewStack();
+    while(1){
+        while(root){
+            printf("node : %d\n", root->data);
+            push(&S, root->right);
+            root = root->left;
+        }
+        if(isEmptyStack(S))
+            break;
+        
+        root = pop(&S);
+    }
+}
+
 int main(){
     struct BinaryTreeNode *BTRoot;
-    BTRoot = createFullBinaryTree_iterative(3);
+    BTRoot = createFullBinaryTree_recursive(3);
 
-    preOrderTraversal_Recursive(BTRoot);
+    preOrderTraversal_Iterative(BTRoot);
 
     return 0;
 }
